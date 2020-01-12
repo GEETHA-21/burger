a5851dc5042ad350eef93e8980819993969deaeb
diya <geethusivaraman@gmail.com>
2020-01-11 20:10:02 -0800

    final update

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "password",
+  host: "ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "ucv4j7th3n30wl7d",
+  password: "b6y3pinyly02av5m",
   database: "burgers_db"
 });
 
